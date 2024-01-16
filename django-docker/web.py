71e77275f408447d745b4bd68aa956599c7ef266
Andrei <andrei.coman@scoalainformala.ro>
2024-01-16 18:59:19 +0200

    Multiple endpoints

--- django-docker/web.py
+++ django-docker/web.py
@@ -4,6 +4,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Salut din Python"
+    return "Salut din Python "
+
+@app.route("/contact")
+def contact():
+    return "Pagina de contact"
+
+@app.route("/help")
+def help():
+    return "Ajutor"
+
+
+times = 0
+
+
+@app.route("/visit")
+def visit():
+    global times
+    times += 1
+    return f"Visited {times}"
+
+
 
 app.run("0.0.0.0")
